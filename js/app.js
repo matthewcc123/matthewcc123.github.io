fbe1e0b35c9601fdd2a905100c795f9e7090fa7f
matthewcc123 <matthew_cc@outlook.com>
2023-09-17 18:31:43 +0700

    Redesign 2023

--- js/app.js
+++ js/app.js
@@ -1,85 +1,268 @@
-function App()
-{
-
-    const burger = document.querySelector(".burger");
-    const navButtons = document.querySelector(".nav-buttons");
-    const burgerIcon = document.querySelector(".burger i");
-
-    let open = false;
-
-    function openNav()
+var gameList = [
     {
-        open = !open;
-        navButtons.classList.toggle('nav-open');
-        
-        //Reset Burger Animation
-        burgerIcon.addEventListener("webkitAnimationEnd", () => {
-            burgerIcon.style.animation = '';
-        })
-
-        //Start Burger Anim
-        burgerIcon.style.animation = `clickAnim .5s`;
-        if (open) {
-            burgerIcon.classList.replace('fa-bars', 'fa-times');
-        } else {
-            burgerIcon.classList.replace('fa-times', 'fa-bars');
-        }
-        
-    }
-    
-    burger.addEventListener('click',openNav);
-
-    //Nav Buttons
-    const nButton = document.querySelectorAll(".nButton");
-    
-    function closeNav()
+        'id' : 'BWRE',
+        'name':'Broken World',
+        'sub' : 'Game Development',
+        'content' : `<div class="slider-wrapper">
+        <a class="slider-item" href="Resources/Images/Games/BWRE/Screenshot.gif" data-lightbox="BWRE"><img src="Resources/Images/Games/BWRE/Screenshot.gif"></a>
+        <a class="slider-item" href="Resources/Images/Games/BWRE/Screenshot-2.png" data-lightbox="BWRE"><img src="Resources/Images/Games/BWRE/Screenshot-2.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/BWRE/Screenshot-3.png" data-lightbox="BWRE"><img src="Resources/Images/Games/BWRE/Screenshot-3.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/BWRE/Screenshot-4.png" data-lightbox="BWRE"><img src="Resources/Images/Games/BWRE/Screenshot-4.png"></a>
+        </div>
+        <br><br>
+        <p>
+        Hack n Slash, Action RPG game. One day, our hero just woke up in the middle of a forest full of monsters, trying to find out what happened.
+        <br><br>
+        This is just a personal project on Highschool and publish it on Itch.io
+        <br><br>
+        This game made with <b>Game Maker Studio : Professional</b> and the Asset made with <b>Photoshop.</b>
+        </p>
+        <br><br>
+        <a href="https://matthew-cc.itch.io/broken-world" target="_blank">
+        <button class="download-btn">Download</button>
+        </a>`,
+    },
     {
-        open = false
-        navButtons.classList.remove('nav-open');
-        burgerIcon.classList.replace('fa-times', 'fa-bars');
+        'id' : 'OTR',
+        'name':'On The Road',
+        'sub' : 'Game Development',
+        'content' : `<div class="slider-wrapper">
+        <a class="slider-item" href="Resources/Images/Games/OTR/Screenshot.png" data-lightbox="OTR"><img src="Resources/Images/Games/OTR/Screenshot.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR/Screenshot-2.png" data-lightbox="OTR"><img src="Resources/Images/Games/OTR/Screenshot-2.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR/Screenshot-3.png" data-lightbox="OTR"><img src="Resources/Images/Games/OTR/Screenshot-3.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR/Screenshot-4.png" data-lightbox="OTR"><img src="Resources/Images/Games/OTR/Screenshot-4.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR/Screenshot-5.png" data-lightbox="OTR"><img src="Resources/Images/Games/OTR/Screenshot-5.png"></a>
+        </div>
+        <br><br>
+        <p>
+        A Top Down Arcade Racing Game, you have to drive your car not off the road at a constant speed with drifting at high speed.
+        <br><br>
+        This is just a personal project on Highschool and publish it on Itch.io
+        <br><br>
+        This game made with <b>Game Maker Studio : Professional</b> and the Asset made with <b>Photoshop.</b>
+        </p>
+        <br><br>
+        <a href="https://matthew-cc.itch.io/on-the-road" target="_blank">
+        <button class="download-btn">Download</button>
+        </a>`,
+    },
+    {
+        'id' : 'OTR2',
+        'name':'On The Road (3D)',
+        'sub' : 'Game Development',
+        'content' : `<div class="slider-wrapper">
+        <a class="slider-item" href="Resources/Images/Games/OTR2/Screenshot.png" data-lightbox="OTR2"><img src="Resources/Images/Games/OTR2/Screenshot.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR2/Screenshot-2.png" data-lightbox="OTR2"><img src="Resources/Images/Games/OTR2/Screenshot-2.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR2/Screenshot-3.png" data-lightbox="OTR2"><img src="Resources/Images/Games/OTR2/Screenshot-3.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR2/Screenshot-4.png" data-lightbox="OTR2"><img src="Resources/Images/Games/OTR2/Screenshot-4.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR2/Screenshot-5.png" data-lightbox="OTR2"><img src="Resources/Images/Games/OTR2/Screenshot-5.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR2/Screenshot-6.png" data-lightbox="OTR2"><img src="Resources/Images/Games/OTR2/Screenshot-6.png"></a>
+        </div>
+        <br><br>
+        <p>
+        This game is made to compete in <b>COMPFEST 11: Indie Game Ignite.</b>
+        <br><br>
+        This is a newer version than the original, there are a lot of new features, especially 3D graphics. Now the game has currency, and you can buy a car or choose a map you like.
+        <br><br>
+        This game made with <b>Unity3D</b> and the Asset made with <b>Blender, Photoshop.</b>
+        </p>
+        <br><br>
+        <a href="https://drive.google.com/drive/folders/1iobR9YfoSiMyyEWjFTFjDaL77LLDvIIZ?usp=sharing" target="_blank">
+        <button class="download-btn">Download</button>
+        </a>`,
+    },
+    {
+        'id' : 'OTR3',
+        'name':'On The Road! (2021)',
+        'sub' : 'Game Development',
+        'content' : `<div class="slider-wrapper">
+        <a class="slider-item" href="Resources/Images/Games/OTR3/Screenshot.png" data-lightbox="OTR3"><img src="Resources/Images/Games/OTR3/Screenshot.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR3/Screenshot-2.png" data-lightbox="OTR3"><img src="Resources/Images/Games/OTR3/Screenshot-2.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR3/Screenshot-3.png" data-lightbox="OTR3"><img src="Resources/Images/Games/OTR3/Screenshot-3.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR3/Screenshot-4.png" data-lightbox="OTR3"><img src="Resources/Images/Games/OTR3/Screenshot-4.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR3/Screenshot-5.png" data-lightbox="OTR3"><img src="Resources/Images/Games/OTR3/Screenshot-5.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR3/Screenshot-6.png" data-lightbox="OTR3"><img src="Resources/Images/Games/OTR3/Screenshot-6.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR3/Screenshot-7.png" data-lightbox="OTR3"><img src="Resources/Images/Games/OTR3/Screenshot-7.png"></a>
+        <a class="slider-item" href="Resources/Images/Games/OTR3/Screenshot-8.png" data-lightbox="OTR3"><img src="Resources/Images/Games/OTR3/Screenshot-8.png"></a>
+        </div>
+        <br><br>
+        <p>
+        <b>On The Road!</b> a remake version of <b>On The Road (3D).</b>
+        <br><br>
+        Decided to remake the game to bring back the original gameplay physics and also upgrade the graphic with optimization so the game can run smoother on low-end devices.
+        <br><br>
+        This game made with <b>Unity 3D</b> and the Asset made with <b>Blender, Photoshop, Illustrator, and Figma</b> for UI Design.
+        </p>
+        <br><br>
+        <a href="https://drive.google.com/drive/folders/1YxEcj2cvBEJ1cn_Gra97-j8azwwx3fgX?usp=sharing" target="_blank">
+        <button class="download-btn">Download</button>
+        </a>`,
+    }
+]
+
+var projectList = [
+    {
+        'id' : 'JNS',
+        'name':'JNS Body Repair',
+        'sub' : 'Web Design',
+        'content' : `<div class="slider-wrapper">
+        <a class="slider-item" href="Resources/Images/Projects/JNS/Screenshot.jpg" data-lightbox="JNS"><img src="Resources/Images/Projects/JNS/Screenshot.jpg"></a>
+        <a class="slider-item" href="Resources/Images/Projects/JNS/Screenshot-2.jpeg" data-lightbox="JNS"><img src="Resources/Images/Projects/JNS/Screenshot-2.jpeg"></a>
+        <a class="slider-item" href="Resources/Images/Projects/JNS/Screenshot-3.jpeg" data-lightbox="JNS"><img src="Resources/Images/Projects/JNS/Screenshot-3.jpeg"></a>
+        <a class="slider-item" href="Resources/Images/Projects/JNS/Screenshot-4.jpeg" data-lightbox="JNS"><img src="Resources/Images/Projects/JNS/Screenshot-4.jpeg"></a>
+        <a class="slider-item" href="Resources/Images/Projects/JNS/Screenshot-5.jpeg" data-lightbox="JNS"><img src="Resources/Images/Projects/JNS/Screenshot-5.jpeg"></a>
+        </div>
+        <br><br>
+        <p>
+        PT. JNS Jaya Motor is a workshop that handles repairs or damage to the vehicle body as well as discoloration and repainting.
+        <br><br>
+        The website designed using <b>Adobe Illustrator and Photoshop</b>, then convert the design to the <b>HTML5, CSS, and JavaScript</b> 
+        for the animations and functions with <b>Visual Studio Code.<b/>
+        </p>
+        <br><br>
+        <a href="http://jnsbodyrepair.com/" target="_blank">
+        <button class="download-btn">Website</button>
+        </a>`,
+    },
+    {
+        'id' : 'MT',
+        'name':`Matt's Tools`,
+        'sub' : '.NET MAUI Blazor Project',
+        'content' : `<div class="slider-wrapper">
+        <a class="slider-item" href="Resources/Images/Projects/MT/Screenshot.jpg" data-lightbox="JNS"><img src="Resources/Images/Projects/MT/Screenshot.jpg"></a>
+        <a class="slider-item" href="Resources/Images/Projects/MT/Screenshot-2.jpg" data-lightbox="JNS"><img src="Resources/Images/Projects/MT/Screenshot-2.jpg"></a>
+        <a class="slider-item" href="Resources/Images/Projects/MT/Screenshot-3.jpg" data-lightbox="JNS"><img src="Resources/Images/Projects/MT/Screenshot-3.jpg"></a>
+        </div>
+        <br><br>
+        <p>
+        Tools I made for better quality of life for my work as <b>Accounts Payable.</b>
+        <br><br>
+        The tools currently are only for merging Unilever Invoices with Tax Invoices and extracting JSON from Rossum using their APIs, but there will be more tools in development.
+        <br><br>
+        At first, I made a prototype with <b>WinForm</b>, then remade it into <b>.NET MAUIBlazor Project</b> for better UI and UX
+        </p>
+        <br><br>
+        <a href="https://github.com/matthewcc123/MattTools" target="_blank">
+        <button class="download-btn">GitHub</button>
+        </a>`,
+    }
+]
+
+var artList = [
+    {'id' : 'john'},
+    {'id' : 'nody'},
+    {'id' : 'workout'},
+    {'id' : 'mage'},
+    {'id' : 'illust'},
+    {'id' : 'mini'},
+    {'id' : 'swift'},
+    {'id' : 's2000'},
+    {'id' : '207'},
+]
+
+//Content Viewer
+var contentOverlay = document.querySelector(".content-overlay");
+var contentWindow = document.querySelector(".content-window");
+var contentCloseButton = document.querySelector(".content-close-btn");
+var scrollContainer;
+
+$(document).ready(function() {
+
+    for (var i in gameList)
+    {
+        var row = `<div class="card" id="game-item">
+                    <img src="Resources/Images/Games/${gameList[i].id}/thumb.png">
+                    </div>`;
+    
+        
+        document.querySelector("#game-list").innerHTML += row;
+
     }
 
-    nButton.forEach((item) => {
-        item.addEventListener('click',closeNav);
-    })
+    for (var i in projectList)
+    {
+        var row = `<div class="card" id="project-item">
+        <img src="Resources/Images/Projects/${projectList[i].id}/thumb.png">
+                    </div>`;
+    
+        
+        document.querySelector("#project-list").innerHTML += row;
 
-    //Scroller
-    $(".nav-buttons a").on("click", function (e) {
-        const href = $(this).attr("href");
-        const string = href.substring(0,1)
+    }
 
-        if (string == "#")
+    for (var i in artList)
+    {
+        var row = `<a class="card" href="Resources/Images/Arts/${artList[i].id}/hd.png" data-lightbox="${artList[i].id}">
+        <img src="Resources/Images/Arts/${artList[i].id}/thumb.png">
+        </a>`;
+    
+        document.querySelector("#art-list").innerHTML += row;
+
+    }
+
+    //Update Content
+    function UpdateContent(id, list)
+    {
+        var title = document.querySelector(".content-title");
+        var sub = document.querySelector(".content-sub");
+        var content = document.querySelector(".content-content");
+
+        title.innerHTML = list[id].name;
+        sub.innerHTML = list[id].sub;
+        content.innerHTML = list[id].content;
+        scrollContainer = document.querySelector(".slider-wrapper");
+
+        if (scrollContainer != null)
         {
-            e.preventDefault();
-            $("html, body").animate({ scrollTop: $(href).offset().top - 120}, 1000);
+            scrollContainer.addEventListener("wheel", (evt) => {
+                evt.preventDefault();
+                scrollContainer.scrollLeft += evt.deltaY;
+                console.log(evt.deltaY);
+            });
         }
-        
-        
-      });
 
-    //Observer
-    const showlist = document.querySelectorAll('.show');
+    }
 
-    const appearOptions = {
-        threshold: 0,
-        rootMargin: "0px 0px -200px 0px"
-    };
+    function CloseWindow()
+    {
+        contentOverlay.classList.remove("active");
+        contentWindow.classList.remove("active");
+    }
 
-    const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("showed");
-                appearOnScroll.unobserve(entry.target);
-            } else {
-                return;
-            }
-        });
-    }, appearOptions);
-
-    showlist.forEach(show => {
-        appearOnScroll.observe(show);
+    //Close Button
+    contentCloseButton.addEventListener("click", ()=> {
+        CloseWindow();
     });
 
-}
+    /*
+    window.onclick = function (event) {
+        if (event.target.contains(contentWindow) && event.target !== contentWindow) {
+            CloseWindow();
+        }
+    }*/
 
-App();
+    var gameItems = document.querySelectorAll("#game-item");
+
+    gameItems.forEach((item, index) => {
+
+        item.addEventListener('click', () => {
+            contentOverlay.classList.add("active");
+            contentWindow.classList.add("active");
+            UpdateContent(index, gameList);
+        });
+
+    });
+
+    var projectItems = document.querySelectorAll("#project-item");
+
+    projectItems.forEach((item, index) => {
+
+        item.addEventListener('click', () => {
+            contentOverlay.classList.add("active");
+            contentWindow.classList.add("active");
+            UpdateContent(index, projectList);
+        });
+
+    });
+
+});
